f575b59caff1a9d3f73d9a576afce9015ba28e06
PichsineeA <happypich.mk@gmail.com>
2023-01-14 16:10:17 +0700

    test

--- src/containers/bill/constants.ts
+++ src/containers/bill/constants.ts
@@ -3,17 +3,17 @@ import { Product } from '@containers/bill/interface'
 export const dataProducts: Product[] = [
   {
     name: 'โค้ก 1.25L',
-    price: 0,
-    quantity: 0,
+    price: 128,
+    quantity: 2
   },
   {
     name: 'เลย์ 1 แพ็ค',
-    price: 0,
-    quantity: 0,
+    price: 25,
+    quantity: 1
   },
   {
     name: 'โก้แก่ 1 ลัง',
-    price: 0,
-    quantity: 0,
+    price: 5,
+    quantity: 12
   }
 ]
